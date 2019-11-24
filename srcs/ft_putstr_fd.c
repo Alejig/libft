3b3e7e275d839d8f28acf5b781b457c23ad11b65
alexandre <alexandre@pc-197.home>
2019-11-24 23:11:15 +0100

    All the functions are now in srcs/

---
+++ srcs/ft_putstr_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aljigmon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/14 14:51:20 by aljigmon          #+#    #+#             */
+/*   Updated: 2019/09/28 15:15:48 by aljigmon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+#include <unistd.h>
+
+void	ft_putstr_fd(char const *s, int fd)
+{
+	if (s == NULL)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
